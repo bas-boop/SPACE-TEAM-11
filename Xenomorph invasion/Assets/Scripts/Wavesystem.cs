107a8d374c97006dfacb0ebc6ed5b4d0f6afea03
SimonImanse <30230@ma-web.nl>
2021-05-26 13:39:44 +0200

    Simon Wavesystem + Enemycode + Scene toegevoegd

---
+++ Xenomorph invasion/Assets/Scripts/Wavesystem.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[System.Serializable]
+public class Wave
+{
+    public string waveName;
+    public EnemyGroup[] DifferentEnemies;
+    public float spawnInterval;
+}
+
+[System.Serializable]
+public class EnemyGroup
+{
+    public GameObject spawnenemy;
+    public int noOfEnemies;
+}
+
+public class Wavesystem : MonoBehaviour
+{
+    public Wave[] waves;
+    public Transform spawnPoint;
+
+    private Wave currentWave;
+    private int currentWaveNumber;
+    private float nextSpawnTime;
+
+    private bool createdList = false;
+    private bool canSpawn;
+    private int y;
+    
+    private int EnemyCountdown;
+    List<GameObject> AllEnemiesList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        GameObject[] spawnEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        y = spawnEnemies.Length;
+        canSpawn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentWave = waves[currentWaveNumber];
+        
+        if (canSpawn == true && nextSpawnTime < Time.time)
+        {
+            EnemyCountdown = AllEnemiesList.Count;
+            //Debug.Log(currentWave);
+            SpawnWave();
+        }
+            
+        GameObject[] totalEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (totalEnemies.Length == y && canSpawn == false && currentWaveNumber + 1 != waves.Length)
+        {
+            currentWaveNumber++;
+            canSpawn = true;
+            createdList = false;
+        }
+    }
+
+    
+    void SpawnWave()
+    {
+        if (createdList == false)
+        {
+            CreateAllEnemyList();
+            createdList = true;
+        }
+        EnemyCountdown--;
+        nextSpawnTime = Time.time + currentWave.spawnInterval;
+        GameObject enemy = Instantiate(GetRandomEnemy(), spawnPoint.position, transform.rotation);
+        enemy.GetComponent<enemycode>().enabled = true;
+        if (EnemyCountdown == 0)
+        {
+            canSpawn = false;
+        }
+    }
+
+    GameObject GetRandomEnemy()
+    {
+        int x = Random.Range(0, AllEnemiesList.Count);
+        GameObject enemy = AllEnemiesList[x];
+        AllEnemiesList.RemoveAt(x);
+        return enemy;
+    }
+
+    void CreateAllEnemyList()
+    {
+        for (int i = 0; i < currentWave.DifferentEnemies.Length; i++)
+        {
+            for (int x = 0; x < currentWave.DifferentEnemies[i].noOfEnemies; x++)
+            {
+                AllEnemiesList.Add(currentWave.DifferentEnemies[i].spawnenemy);
+            }
+        }
+        foreach (GameObject g in AllEnemiesList) 
+        {
+            Debug.Log(g);
+        }
+        //Debug.Log(AllEnemiesList);
+    }
+}
---
+++ Xenomorph invasion/Assets/Scripts/enemycode.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TO DO: adjusting health, damage and speed for different enemies
+
+public class enemycode : MonoBehaviour
+{
+    private int damage = 1;
+    private int health = 100;
+    private float speed = 30;
+    //private float waitTime;
+    //private float startwaitTime = 5;
+    private float distanceCheckpoint = 0.005f;
+
+    public Transform spawnSpot;
+    public Transform endPoint;
+
+    public Transform[] movetospots;
+    private Transform currentWave;
+
+    private int index = 0;
+    private int y = 0;
+    private int location;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //waitTime = startwaitTime;
+        //spawnAtSpot = Random.Range(0, spawnSpot.Length);
+
+        if (Vector3.Distance(transform.position, spawnSpot.position) < distanceCheckpoint)
+        {
+            location = 0;
+        }
+        else
+        {
+            Debug.Log("enemy niet bij spawnpoint");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (y == 0)
+        {
+            y = movetospots.Length - 1;
+        }
+
+        if (location == index && index<=y)
+        {
+            currentWave = movetospots[index];
+            transform.position = Vector3.MoveTowards(transform.position, currentWave.position, speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, currentWave.position) < distanceCheckpoint)
+            {
+                /*if (waitTime <= 0)
+                {
+                    waitTime = startwaitTime;
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }*/
+                
+                //Debug.Log("locatie " + x);
+                index++;
+                location = index;
+                
+
+            }
+            
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.position, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, endPoint.position) < distanceCheckpoint)
+            {
+                /*if (waitTime <= 0)
+                {
+                    waitTime = startwaitTime;
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }*/
+                Finish(damage);
+            }
+        }
+
+        void Finish(int y)
+        {
+            //Player.health-y;
+            Debug.Log("-" + y + "hp");
+            Destroy(gameObject);
+
+        }
+        
+
+    }
+
+    /*public void SetSpeed(float s)
+    {
+        speed = s;
+    }
+
+    public void SetDamage(int d)
+    {
+        damage = d;
+    }
+
+    public void SetHealth(int h)
+    {
+        health = h;
+    }*/
+}
