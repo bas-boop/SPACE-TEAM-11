d13989d283ae04f23279061661cea8cd3ad0e275
Joyce Hollander <31888@ma-web.nl>
2021-06-01 10:35:51 +0200

    added a lot and changed option menu most buttons work now and added new scenes

--- Xenomorph invasion/Assets/Scripts/OptionMenuGame.cs
+++ Xenomorph invasion/Assets/Scripts/OptionMenuGame.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class OptionMenuGame : MonoBehaviour
 {
-    public void PlayGame()
+    public void BackToGame()
     {
         SceneManager.LoadScene("Test Simon");
-    }
-
-    public static void CallOptions()
-    {
-        SceneManager.LoadScene("OptionsMenu");
     }
 }
---
+++ Xenomorph invasion/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject pauseMenuScene;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuScene.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuScene.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void LoadOptions()
+    {
+        SceneManager.LoadScene("GameOptionsMenu");
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
