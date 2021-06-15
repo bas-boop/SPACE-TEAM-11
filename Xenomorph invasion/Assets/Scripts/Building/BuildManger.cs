ba18bb4f5f7f193cc4a0b9a0ab987b1d949bb9f2
Bas <30810@ma-web.nl>
2021-06-15 15:25:00 +0200

    begin aan de buildmanger

---
+++ Xenomorph invasion/Assets/Scripts/Building/BuildManger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BuildManger : MonoBehaviour
+{
+    public static BuildManger instance;
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild ()
+    {
+        return turretToBuild;
+    }
+}
--- Xenomorph invasion/Assets/Scripts/Building/Node.cs
+++ Xenomorph invasion/Assets/Scripts/Building/Node.cs
@@ -4,6 +4,8 @@ public class Node : MonoBehaviour
 {
     public Color hoverColor;
 
+    private GameObject turret;
+
     private Renderer rend;
     private Color startColor;
 
@@ -11,6 +13,17 @@ public class Node : MonoBehaviour
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Can't build there! - met UI dit laten zien");
+            return;
+        }
+
+        //bluid a turret
     }
 
     void OnMouseEnter()
